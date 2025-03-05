61c2b11f4e79b8693e57970e03ca8abb6cea0f4b
diogo varaschin <diogovaraschin@outlook.com>
2025-03-04 21:21:06 -0300

    create database using sqlite

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
 db = SQLAlchemy(app)
 
+#modelagem
+#produto: id, name, price, description
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
 #def rota raiz pag inicial e a funcao
 @app.route('/')
 def hello_world():
