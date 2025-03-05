36cceddba78ee1e137450a08cc6676dcb1d81e2c
diogo varaschin <diogovaraschin@outlook.com>
2025-03-04 21:12:59 -0300

    alterações swagger.yaml e app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask;
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__);
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+
+db = SQLAlchemy(app)
 
 #def rota raiz pag inicial e a funcao
 @app.route('/')
